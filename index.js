97da498b6d423093d75567f87fbb844d42fb21fc
도채원 <ehcodnjs123@gmail.com>
2020-01-10 14:59:39 +0900

    EJS로 Dynamic Website 만들기

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 // index.js
-﻿
+
 ﻿var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public')); // 1
+app.set("view engine","ejs"); // 1
+app.use(express.static(__dirname + '/public'));
 
-var port = 3000;
-app.listen(port, function(){
-  console.log('server on! http://localhost:'+port);
+// 2
+app.get("/hello", function(req,res){
+  res.render("hello", {name:req.query.nameQuery});
+});
+
+// 3
+app.get("/hello/:nameParam", function(req,res){ 
+  res.render("hello", {name:req.params.nameParam});
+});
+
+app.listen(3000, function(){
+  console.log('Server On!');
 });
